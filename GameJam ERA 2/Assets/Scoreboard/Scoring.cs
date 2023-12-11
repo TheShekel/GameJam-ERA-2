87bcd6cb7bf803a54f450e00c22f18b41e31e274
TheShekel <150011326+TheShekel@users.noreply.github.com>
2023-12-11 21:18:09 +0000

    Figuring out Scoring

--- GameJam ERA 2/Assets/Scoreboard/Scoring.cs
+++ GameJam ERA 2/Assets/Scoreboard/Scoring.cs
@@ -1,26 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 
 
-public class Scoring : MonoBehaviour
+public class Scoring : MonoBehaviour, IAddToScore
 {
     public float currentScore;
     public Text ScoreNumber;
-    public float addThisScore;
+
+    UnityEvent event_scoringCheck;
+
+    public void AddToScore(float addToScore)
+    {
+        throw new System.NotImplementedException();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (event_scoringCheck == null)
+            event_scoringCheck = new UnityEvent();
     }
 
     // Update is called once per frame
     void Update()
     {
         ScoreNumber.text = "Score: " + Mathf.Round(currentScore);
-        currentScore = currentScore + addThisScore;
+        
+    }
+
+    public void AddToScore()
+    {
+        currentScore = currentScore + IAddToScore;
     }
 }
